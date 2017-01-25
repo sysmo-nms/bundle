20e8047bb0738e221a3a379e110dd10da3f884fa
Sébastien Serre <sserre.bx@gmail.com>
2017-01-25 19:50:18 +0100

    add debug info

--- bundle.cmd
+++ bundle.cmd
@@ -2,35 +2,35 @@
 mkdir _build
 
 :: Build installer bundle
-@set ressources=%USERPROFILE%\SYSMO_NMS_RESSOURCES
-@set CORE_VERSION=2.0.1
-@set OPERATOR_VERSION=2.0.3
-@set BUNDLE_VERSION=2.0
-@if not exist %ressources% mkdir %ressources%
+set ressources=%USERPROFILE%\SYSMO_NMS_RESSOURCES
+set CORE_VERSION=2.0.1
+set OPERATOR_VERSION=2.0.3
+set BUNDLE_VERSION=2.0
+if not exist %ressources% mkdir %ressources%
 
-@if not DEFINED PLATFORM set PLATFORM=x64
+if not DEFINED PLATFORM set PLATFORM=x64
 
-@if "%PLATFORM%" == "Win32" (
-    @set WIX_ARCH=x86
-    @set COMMON_ARCH=i586
-    @set VCREDIST_INSTALLER_2010=%ressources%\2015_vcredist_x86.exe
-    @set VCREDIST_INSTALLER_2015=%ressources%\2010_vcredist_x86.exe
-    @set JAVA_INSTALLER=%ressources%\jre8_586.exe
-    @set vcredist_location_2010="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2010_vc_redist.x86.exe"
-    @set vcredist_location_2015="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2015_vc_redist.x86.exe"
-    @set java_location="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/jre-8u111-windows-i586.exe"
+if "%PLATFORM%" == "Win32" (
+    set WIX_ARCH=x86
+    set COMMON_ARCH=i586
+    set VCREDIST_INSTALLER_2010=%ressources%\2015_vcredist_x86.exe
+    set VCREDIST_INSTALLER_2015=%ressources%\2010_vcredist_x86.exe
+    set JAVA_INSTALLER=%ressources%\jre8_586.exe
+    set vcredist_location_2010="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2010_vc_redist.x86.exe"
+    set vcredist_location_2015="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2015_vc_redist.x86.exe"
+    set java_location="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/jre-8u111-windows-i586.exe"
 ) else (
-    @set WIX_ARCH=x64
-    @set VCREDIST_INSTALLER_2010=%ressources%\2015_vcredist_x64.exe
-    @set VCREDIST_INSTALLER_2015=%ressources%\2010_vcredist_x64.exe
-    @set JAVA_INSTALLER=%ressources%\jre8_64.exe
-    @set vcredist_location_2010="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2010_vc_redist.x64.exe"
-    @set vcredist_location_2015="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2015_vc_redist.x64.exe"
-    @set java_location="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/jre-8u111-windows-x64.exe"
+    set WIX_ARCH=x64
+    set VCREDIST_INSTALLER_2010=%ressources%\2015_vcredist_x64.exe
+    set VCREDIST_INSTALLER_2015=%ressources%\2010_vcredist_x64.exe
+    set JAVA_INSTALLER=%ressources%\jre8_64.exe
+    set vcredist_location_2010="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2010_vc_redist.x64.exe"
+    set vcredist_location_2015="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/msvc2015_vc_redist.x64.exe"
+    set java_location="https://github.com/sysmo-nms/bundle/releases/download/RESSOURCES/jre-8u111-windows-x64.exe"
 )
 
-@set core_msi_location="https://github.com/sysmo-nms/sysmo-core/releases/download/%CORE_VERSION%/zcore-%COMMON_ARCH%.msi"
-@set operator_msi_location="https://github.com/sysmo-nms/sysmo-operator/releases/download/%OPERATOR_VERSION%/operator-%COMMON_ARCH%.msi"
+set core_msi_location="https://github.com/sysmo-nms/sysmo-core/releases/download/%CORE_VERSION%/zcore-%COMMON_ARCH%.msi"
+set operator_msi_location="https://github.com/sysmo-nms/sysmo-operator/releases/download/%OPERATOR_VERSION%/operator-%COMMON_ARCH%.msi"
 
 if not exist "%VCREDIST_INSTALLER_2010%" curl -fSL -o "%VCREDIST_INSTALLER_2010%" %vcredist_location_2010%
 copy /y %VCREDIST_INSTALLER_2010% _build\vcredist_2010.exe
@@ -47,8 +47,8 @@ curl -fSL -o "%CORE_INSTALLER%" %core_msi_location%
 curl -fSL -o "%OPERATOR_INSTALLER%" %operator_msi_location%
 
 :: Wix bundle
-@set PATH=C:\Program Files (x86)\Wix Toolset v3.10\bin;%PATH%
-@set wix_opts= -v -nologo -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension -ext WixFirewallExtension -ext WixUIExtension
+set PATH=C:\Program Files (x86)\Wix Toolset v3.10\bin;%PATH%
+set wix_opts= -v -nologo -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension -ext WixFirewallExtension -ext WixUIExtension
 
 @echo "Run candle.exe"
 candle.exe %wix_opts% -arch %WIX_ARCH% -o _build\bundle.wixobj bundle.wxs
